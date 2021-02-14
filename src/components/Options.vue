f6f84cb8cd97e5fd8cbf4a59fdec4e7ee8337041
tomtidswell <tomtidswell@gmail.com>
2021-02-14 12:14:38 +0000

    Options input elements

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="options">
-    <div v-for="(option, index) in options" :key="option">
-      <input type="radio" :id="`option-${index}`" :value="option" v-model="choice" @change="$emit('choice', option)">
-      <label :for="`option-${index}`">{{option}}</label>
-    </div>
+    <label v-for="(option, index) in options" :key="option" class="option">
+      <input type="radio" :value="option" v-model="choice" @change="$emit('choice', option)">
+      <span class="radio"></span>
+      {{option}}
+    </label>
   </div>
 </template>
 
@@ -30,7 +31,33 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.thinking {
+.option {
   display: flex;
+  align-items: center;
+  padding: 15px 0;
+  border-bottom: 1px solid blue;
+  &:last-child{
+    border-bottom: 0;
+  }
+  input{
+    display: none;
+  }
+}
+.radio{
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  margin: 0 8px 2px 0;
+  border: 2px solid grey;
+  background-color: white;
+  border-radius: 100%;
+  &:hover{
+    border-color: grey;
+    background-color: grey;
+  }
+}
+input:checked ~ .radio {
+  border-color: blue;
+  background-color: blue;
 }
 </style>
